8b3341a06a273a3e0553a9ed49691dff31c86c8e
gauravKothari <kotharigaurav6@gmail.com>
2025-08-21 17:38:13 +0530

    advance

---
+++ 6 Advance_330_servlet/src/java/ServletDemo5.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ServletDemo5 extends HttpServlet{
+    
+    protected void processRequest(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        ServletConfig config = getServletConfig();
+        String  driverName = config.getInitParameter("driver");
+        out.println("Driver name : "+driverName);
+        
+         ServletContext context = getServletContext();
+        String  projectName = context.getInitParameter("project");
+        out.println("<br>Project name : "+projectName);
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException,IOException{
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request , HttpServletResponse response) throws ServletException,IOException{
+        processRequest(request, response);
+    }
+}
---
+++ 7 Advance_330_servletCRUD/src/java/controller/Registration.java
@@ -0,0 +1,27 @@
+package controller;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+public class Registration extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+ 
+        }catch(Exception e){
+            System.out.println("Exception :  "+e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
